fa3062a6ebcd6910103f3938193c277d5d4d947f
allyciajoshin <149560070+allyciajoshin@users.noreply.github.com>
2023-11-03 10:34:55 +0800

    Add files via upload

---
+++ QUIZ51021001/src/quiz51021001/QUIZ51021001.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz51021001;
+
+/**
+ *
+ * @author Asus
+ */
+public class QUIZ51021001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new menu();
+        
+    }
+    
+}
